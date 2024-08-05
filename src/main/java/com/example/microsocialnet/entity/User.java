14cbec6e64122185e9f4ca28e30c336022d4208e
Marina K <marina.frontend.rostov@gmail.com>
2024-08-05 14:14:01 +0300

    Updated User Entity. Added new fields.

--- src/main/java/com/example/microsocialnet/entity/User.java
+++ src/main/java/com/example/microsocialnet/entity/User.java
@@ -1,20 +1,59 @@
 package com.example.microsocialnet.entity;
 
+import org.springframework.data.annotation.CreatedDate;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDate;
+import java.util.Date;
 
 @Entity
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @CreatedDate
+    private LocalDate created;
+
+    private String email;
     private String firstName;
     private String lastName;
 
+    private Date birthdate;
+
+    private String gender;
+
+    private String avatar;
+
+    private String info;
+
+    public User() {
+    }
+
+    public User(Long id, LocalDate created, String email, String firstName, String lastName, Date birthdate, String gender, String avatar, String info) {
+        this.id = id;
+        this.created = created;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthdate = birthdate;
+        this.gender = gender;
+        this.avatar = avatar;
+        this.info = info;
+    }
+
     public Long getId() {
         return id;
+    }
+
+    public LocalDate getCreated() {
+        return created;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     public String getFirstName() {
@@ -25,20 +64,34 @@ public class User {
         return lastName;
     }
 
-    public User() {
+    public Date getBirthdate() {
+        return birthdate;
     }
 
-    public User(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+    public String getGender() {
+        return gender;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public String getInfo() {
+        return info;
     }
 
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
+                ", created=" + created +
+                ", email='" + email + '\'' +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
+                ", birthdate=" + birthdate +
+                ", gender='" + gender + '\'' +
+                ", avatar='" + avatar + '\'' +
+                ", info='" + info + '\'' +
                 '}';
     }
 }
